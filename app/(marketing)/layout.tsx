76c5cdca75691575d79670aa2b6ee82e9df598e4
danny891221 <dan891221@gmail.com>
2023-12-17 16:54:56 +0800

    Basic Setup

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,9 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full">
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
